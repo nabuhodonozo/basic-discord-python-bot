5d3db05302a606e2cc83c652f003e137a133c4e2
Tomasz Czarnecki <tomasz.czarnecki@cognifide.com>
2019-07-01 00:05:59 +0200

    Added deleting recent messages in bulk.

--- bot.py
+++ bot.py
@@ -7,4 +7,9 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(bot.user.name + " initialized!")
 
+@bot.command(pass_context=True)
+async def purge(ctx, limit: int):
+    deleted = await ctx.message.channel.purge(limit=limit+1)
+    print('Deleted {} message(s), by {}'.format(len(deleted), ctx.message.author))
+
 bot.run(token)
